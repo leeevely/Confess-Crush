5ca1fb1900d0e17e5d28e294e4c34971cdded10b
leeevely <99496793+leeevely@users.noreply.github.com>
2022-02-12 19:52:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Valentine Đầu Tiên",
-    introTitle: 'Lương Giang Ngọc',
+    introTitle: 'Mèo Nhỏ Đáng Yêu❤',
     introDesc: `Hôm nay là ngày mà bọn yêu nhau hay tặng quà nhau
     Thế nên với tư cách người yêu em
     Muốn cho em 1 điều đặc biệt mặc dù mình ở xa nhau
     Đừng chê nha!`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    title: 'Em có yêu con bé ngồi làm cái trò con bò này cho em khonggg ❤',
+    desc: 'Dám bấm không đi nhaaaa',
+    btnYes: 'Có ❤',
+    btnNo: 'Không nha (Đố em bắt được tui) :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình❤',
+    btnReply: 'Gửi cho ny bạn nè <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    mess: 'Tui biết mà❤. Tui cũng yêu bạn lắm đóoo 😘😘',
+    messDesc: 'Hi vọng Valentine này sẽ là ngày đặc biệt nhất từ trước đến giờ của em. Yêu em ❤',
+    btnAccept: 'Đặc biệt chắc rùi <3',
+    messLink: 'https://www.facebook.com/messages/t/100009197149948' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
